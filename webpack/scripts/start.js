87e2bf668300dce94b295e3ea56e5942e270b382
kete <jack.ke@ximalaya.com>
2021-07-01 16:05:56 +0800

    fix(shell): fix shell async

--- webpack/scripts/start.js
+++ webpack/scripts/start.js
@@ -11,13 +11,39 @@ log(chalk.red('servers starting....'));
 // 前端代码构建 服务进程
 // const feCodeWatchProcess = spawn('npm', ['run', 'fe:watch'], {
 const feCodeWatchProcess = spawn('npm', ['run', 'wds:watch'], {
-  stdio: 'inherit',
   shell: process.platform === 'win32',
+});
+
+let wdsDone = false;
+let svrDone = false;
+
+feCodeWatchProcess.stdout.on('data', (data) => {
+  let str = data.toString();
+  if (str.indexOf('wds server compile done') > -1) {
+    wdsDone = true;
+    if (wdsDone && svrDone) {
+      startNodeServer();
+    }
+  } else {
+    console.log(str);
+  }
 });
 
 //服务端代码监控和编译进程
 const svrCodeWatchProcess = spawn('npm', ['run', 'svr:watch'], {
   shell: process.platform === 'win32',
+});
+
+svrCodeWatchProcess.stdout.on('data', (data) => {
+  let str = data.toString();
+  if (str.indexOf(constantCode.SVRCODECOMPLETED) > -1) {
+    svrDone = true;
+    if (wdsDone && svrDone) {
+      startNodeServer();
+    }
+  } else {
+    console.log(str);
+  }
 });
 
 let nodeServerProcess = null;
@@ -32,14 +58,8 @@ const startNodeServer = () => {
 
 function print(data) {
   let str = data.toString();
-  if (str.indexOf(constantCode.SVRCODECOMPLETED) > -1) {
-    startNodeServer();
-  } else {
-    console.log(str);
-  }
+  console.log(str);
 }
-
-svrCodeWatchProcess.stdout.on('data', print);
 
 const killChild = () => {
   svrCodeWatchProcess && svrCodeWatchProcess.kill();
